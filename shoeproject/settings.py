e86c7424436f6fd5af6e65de24ccc3c53c16ed45
Rayees <mrc53445@gmail.com>
2026-03-29 20:48:02 +0530

    Change DEBUG setting to False
    Set DEBUG to False for production environment.

--- shoeproject/settings.py
+++ shoeproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1@$%v@r$fqiyrke&1=)bne-4%^^0cdv0dpcf2l2ji7mtwbw196'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
